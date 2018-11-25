96d906fc2531ff2eb47d0b5c27febc81aa6e2085
YWW932888 <1063331410@qq.com>
2018-11-25 13:11:15 +0800

    master

--- test1/src/main/java/GitTest1.java
+++ test1/src/main/java/GitTest1.java
@@ -2,5 +2,6 @@ public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Master");
+        System.out.println("master修改");
     }
 }
