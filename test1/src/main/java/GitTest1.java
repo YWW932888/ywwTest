7bb2d1d324a5f8653e6f842a3d03f6add183be1b
yangweiwei9 <yangweiwie9@jd.com>
2018-11-26 15:54:36 +0800

    测试4

--- test1/src/main/java/GitTest1.java
+++ test1/src/main/java/GitTest1.java
@@ -1,10 +1,5 @@
 public class GitTest1 {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
-        System.out.println("Master");
-        System.out.println("master修改");
-        System.out.println("master修改第二次");
-        System.out.println("master修改第三次");
-        System.out.println("master修改第四次");
+        System.out.println("测试4");
     }
 }
